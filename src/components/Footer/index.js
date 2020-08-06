25762ee07a1878ec8d9574156ce047d9870d23de
Bruno Oliveira <uffbruno@gmail.com>
2020-08-06 12:02:02 -0300

    Footer atualizado com link para portfolio.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,11 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Orgulhosamente criado por
+        {' '}
+        <a href="https://uffbruno.github.io/portfolio/">Bruno Oliveira</a>
+        {' '}
+        durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
